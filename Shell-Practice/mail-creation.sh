7ccfbd35873da69254c786f153d0507cbbe8eb3f
ManojKRISHNAPPA <107530108+ManojKRISHNAPPA@users.noreply.github.com>
2025-09-25 09:52:07 +0530

    Update mail-creation.sh

--- Shell-Practice/mail-creation.sh
+++ Shell-Practice/mail-creation.sh
@@ -3,9 +3,10 @@
 # Update and install msmtp
 sudo apt-get update
 sudo apt-get install -y msmtp msmtp-mta ca-certificates
-
+sudo apt-get install -y bsd-mailx
 # Create the msmtp configuration directory
 mkdir -p ~/.msmtp
+chmod 777 ~/.msmtp
 
 # Create the msmtp configuration file
 cat <<EOF > ~/.msmtprc
@@ -22,7 +23,7 @@ host           smtp.gmail.com
 port           587
 from           manojdevopstest@gmail.com
 user           manojdevopstest@gmail.com
-password       lerkbxagiltjnfos
+password       
 
 # Set a default account
 account default : gmail
@@ -31,8 +32,8 @@ EOF
 # Set the correct permissions for the msmtp configuration file
 chmod 600 ~/.msmtprc
 
-# Create a log file for msP0+r\P0+r\P0+r\P0+r\P0+r\P0+r\mtp
+# Create a log file for
 touch ~/.msmtp/msmtp.log
-chmod 600 ~/.msmtp/msmtp.log
+chmod 777 ~/.msmtp/msmtp.log
 
 echo "msmtp configuration completed. You can now send emails using the configured Gmail account."
